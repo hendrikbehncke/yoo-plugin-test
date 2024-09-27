9b56092aaa0991c955f8efe8419408f58690a671
hendrikbehncke <hendrik.behncke@gmx.de>
2024-09-27 12:29:38 +0200

    add workflow test

---
+++ build/joomla/myPlugin.php
@@ -0,0 +1,30 @@
+<?php
+
+defined('_JEXEC') || exit();
+
+use Joomla\CMS\Application\CMSApplication;
+use Joomla\CMS\Plugin\CMSPlugin;
+use YOOtheme\Application;
+
+class plgSystemMyPlugin extends CMSPlugin
+{
+    /**
+     * @var CMSApplication
+     */
+    public $app;
+
+    /**
+     * Use 'onAfterInitialise' event.
+     */
+    public function onAfterInitialise()
+    {
+        // Check if the YOOtheme app class exists
+        if (!class_exists(Application::class, false)) {
+            return;
+        }
+
+        // Load module from the same directory
+        $app = Application::getInstance();
+        $app->load(__DIR__ . '/modules/*/bootstrap.php');
+    }
+}
---
+++ build/wordpress/myPlugin.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Plugin Name: er
+ * Plugin URI: {{AUTHORURL}}
+ * Description: {{DESCRIPTION}}
+ * Author: {{AUTHOR}}
+ * Version: {{VERSION}}
+ * Author URI: {{AUTHORURL}}
+ * License: {{LICENSE}}
+ * Text Domain: myPlugin
+ * Update URI: {{UPDATEURI}}/update.json
+ * Requires at least: {{WORDPRESSMINIMUM}}
+ * Requires PHP: {{PHPMINIMUM}}
+ */
+
+defined('ABSPATH') || exit();
+
+use YOOtheme\Application;
+
+add_action('after_setup_theme', function () {
+    // Check if the YOOtheme app class exists
+    if (!class_exists(Application::class, false)) {
+        return;
+    }
+
+    // Load module from the same directory
+    $app = Application::getInstance();
+    $app->load(__DIR__ . '/modules/*/bootstrap.php');
+});
+
+add_filter('update_plugins_{{UPDATEHOST}}', function($update, $plugin_data, $plugin_file, $locales) {
+    if ($plugin_file == plugin_basename(__FILE__)) {
+		$request = wp_remote_get($plugin_data['UpdateURI']);
+		$request_body = wp_remote_retrieve_body( $request );
+		$update = json_decode( $request_body, true );
+	}
+
+	return $update;
+},10, 4);
